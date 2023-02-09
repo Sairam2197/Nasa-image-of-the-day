e608dcc31a1c14dfafd38b01cda2f944344b59db
Sairam <mstrthedespicable@gmail.com>
2023-02-09 11:48:07 +0530

    added image source, created a webpage and published the image

---
+++ README.md
@@ -0,0 +1,4 @@
+# What does this app do
+
+This takes image from Nasa's Image of the day site to display 
+in here, you can also download the image.
---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import streamlit as st
+
+url = "https://api.nasa.gov/planetary/apod?" \
+      "date&" \
+      "api_key=JcWcslsByBZaJXepvcECbMwDay1ucfOBCoGvNTnm"
+
+# getting request
+request = requests.get(url)
+# making request as a dictionary
+content = request.json()
+
+# Getting title, image and explanation from the content dictionary
+st.header(content["title"])
+st.image(content["hdurl"])
+st.button("Download HD Image") # added download button
+st.write(content["explanation"])
+
+# download function
+# Getting image URL from the content dictionary
+get_image = requests.get(content["hdurl"], stream=True)
+# Set decode_content value to True, otherwise the downloaded image file's size will be zero
+get_image.raw.decode_content = True
+if st.button:
+    with open("image.jpg", "wb") as file:
+        file.write(get_image.content)
