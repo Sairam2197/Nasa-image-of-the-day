2236240c3afe79e2e137b4cf864ab1313e8855b8
Sairam <mstrthedespicable@gmail.com>
2023-02-09 12:21:25 +0530

    removed download button but kept download funtion to work in future

--- main.py
+++ main.py
@@ -12,20 +12,14 @@ content = request.json()
 
 # Getting title, image and explanation from the content dictionary
 st.header(content["title"])
-filename = content["title"]
 st.image(content["url"])
-st.download_button(label="Download Image",
-                   data=content["hdurl"],
-                   file_name=f"{filename}.jpg")
 st.write(content["explanation"])
 
 # download function
 # Getting image URL from the content dictionary
-get_image = requests.get(content["hdurl"], stream=True)
+get_image = requests.get(content["url"], stream=True)
 # Set decode_content value to True, otherwise the downloaded image file's size will be zero
 get_image.raw.decode_content = True
-if st.button:
-    with open("image.jpg", "wb") as file:
-        file.write(get_image.content)
+
 
 
